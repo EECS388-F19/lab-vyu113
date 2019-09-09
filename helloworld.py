629e5b8428a1d467d8bc90e775921ac96175c6a4
viviyu <viviyu@umich.edu>
2019-09-09 14:51:31 -0400

    Pushing python code

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import randint
+a = randint(1, 100)
+b = randint(1, 100)
+
+answer = a + b
+
+print("Vivian")
+print(a)
+print(b)
+print("Sum =" + str(answer))
+print("Average =" + str(answer/2.0))
